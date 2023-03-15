6d147a7bd7fabced5da726ec8dce8d2ebdf4ba60
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2023-03-15 20:08:53 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,46 +1,34 @@
-import json
 import requests
+import json
 
-# Discord webhook URL
-discord_webhook_url = "https://discord.com/api/webhooks/1085439407512891423/KQILUDIPVggOdMGVuXl5BmMj4j7Yer-nk-W3FrsXBiHuEMBgZyM_mHixIFLuj-0VHKrZ"
+webhook_url = 'https://discord.com/api/webhooks/1085439407512891423/KQILUDIPVggOdMGVuXl5BmMj4j7Yer-nk-W3FrsXBiHuEMBgZyM_mHixIFLuj-0VHKrZ'
 
-def send_discord_message(title, user):
-    # Create the message payload
-    message = {
-        "username": "Plex Bot",
-        "avatar_url": "https://i.imgur.com/g3ZvNSm.png",
-        "embeds": [
-            {
-                "title": f"{user} is now watching {title}",
-                "color": 6570404
-            }
-        ]
-    }
-    
-    # Send the message to the Discord webhook
-    response = requests.post(discord_webhook_url, json=message)
-    
-    # Check the response status code
-    if response.status_code == 204:
-        print("Discord message sent successfully")
-    else:
-        print(f"Error sending Discord message: {response.text}")
+# Define your message content
+message = {
+    "content": "Hello, world!",
+    "embeds": [{
+        "title": "Embed Title",
+        "description": "Embed Description",
+        "color": 16711680, # This sets the color of the embed to red
+        "footer": {
+            "text": "Embed Footer"
+        },
+        "fields": [{
+            "name": "Field Name",
+            "value": "Field Value",
+            "inline": False
+        }]
+    }]
+}
 
-def handle_webhook(payload):
-    # Extract the relevant data from the webhook payload
-    event = payload['event']
-    user = payload['Account']['title']
-    
-    # Check if the event is a "media.play" event for a movie
-    if event['type'] == "media.play" and event['Metadata']['type'] == "movie":
-        title = event['Metadata']['title']
-        send_discord_message(title, user)
+# Convert the message dictionary to JSON
+payload = json.dumps(message)
 
-# Listen for incoming Plex webhooks
-while True:
-    try:
-        # Wait for incoming webhook data
-        payload = json.loads(input())
-        handle_webhook(payload)
-    except:
-        pass
+# Send the message to the webhook URL
+response = requests.post(webhook_url, data=payload, headers={'Content-Type': 'application/json'})
+
+# Check if the message was sent successfully
+if response.status_code == 204:
+    print('Message sent successfully!')
+else:
+    print('Error sending message: %s' % response.text)
