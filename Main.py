c9cb915670189cb7603d2304abc28d7c50e2bbea
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2023-03-25 00:52:08 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import os
 import time
 import json
 import getpass
+
 username = getpass.getuser()
 init()
 os.system("title Discord Fucker V1")
@@ -15,7 +16,7 @@ time.sleep(.2)
 print(Fore.GREEN + "[CONSOLE]: Welcome, ", username,"!")
 time.sleep(.2)
 print(Fore.BLUE + "[CONSOLE]: Command Prompt Loaded")
-webhookforbot = input("Please insert the bots webhook url >>>")
+webhookforbot = input("Please insert the bot's webhook URL >>>")
 code_exe = input(">>>")
 
 # Exit loading phase and onto commands
@@ -26,21 +27,22 @@ if code_exe == "1":
     if response.status_code == 204:
         print("Webhook deleted successfully.")
         time.sleep(2)
-else:
-    print("Error deleting webhook. Status code:", response.status_code)
-    time.sleep(2)
-
-if code_exe == "2":
+    else:
+        print("Error deleting webhook. Status code:", response.status_code)
+        time.sleep(2)
+elif code_exe == "2":
     msgsent = input("Input Message >>>")
     webhook_url = webhookforbot
     message = msgsent
     payload = {'content': message}
     json_payload = json.dumps(payload)
     response = requests.post(webhook_url, data=json_payload, headers={'Content-Type': 'application/json'})
-if response.status_code == 204:
-    print('Message sent successfully')
-    time.sleep(1)
+    if response.status_code == 204:
+        print('Message sent successfully')
+        time.sleep(1)
+    else:
+        print(f'Failed to send message. Response status code: {response.status_code}')
+        time.sleep(1)
 else:
-    print(f'Failed to send message. Response status code: {response.status_code}')
-    time.sleep(1)
-    
+    print("Invalid option. Please choose either '1' or '2'.")
+    time.sleep(2)
