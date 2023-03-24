4f0323e51546d99dce47e7ff1ca8e19bdd3bb6b6
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2023-03-25 00:47:20 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,6 +8,8 @@ username = getpass.getuser()
 init()
 os.system("title Discord Fucker V1")
 
+# Loading Phase
+
 print(Fore.GREEN + "[CONSOLE]: Discord Fucker Has Loaded!")
 time.sleep(.2)
 print(Fore.GREEN + "[CONSOLE]: Welcome, ", username,"!")
@@ -15,7 +17,10 @@ time.sleep(.2)
 print(Fore.BLUE + "[CONSOLE]: Command Prompt Loaded")
 webhookforbot = input("Please insert the bots webhook url >>>")
 code_exe = input(">>>")
-if code_exe == "1":
+
+# Exit loading phase and onto functions
+
+def deletewebhook(requests):
     webhook_url = webhookforbot
     response = requests.delete(webhook_url)
     if response.status_code == 204:
@@ -24,18 +29,24 @@ if code_exe == "1":
 else:
     print("Error deleting webhook. Status code:", response.status_code)
     time.sleep(2)
+    
+def sendmsg(requests):
+    msgsent = input("Input Message >>>")
+    webhook_url = webhookforbot
+    message = msgsent
+    payload = {'content': message}
+    json_payload = json.dumps(payload)
+    response = requests.post(webhook_url, data=json_payload, headers={'Content-Type': 'application/json'})
+if response.status_code == 204:
+    print('Message sent successfully')
+    time.sleep(1)
+else:
+    print(f'Failed to send message. Response status code: {response.status_code}')
+    time.sleep(1)
+    
+if code_exe == "1":
+    deletewebhook()
 
 if code_exe == "2":
-    name = input("New Discord Webhook Name >>>")
-    new_name = name
-    payload = {'name': new_name}
-    json_payload = json.dumps(payload)
-    response = requests.patch(webhook_url, data=json_payload, headers={'Content-Type': 'application/json'})
-    if response.status_code == 200:
-        print(f'Webhook name updated to "{new_name}"')
-        time.sleep(2)
-else:
-    print(f'Failed to update webhook name. Response status code: {response.status_code}')
-    time.sleep(2)
-
+    sendmsg()
     
