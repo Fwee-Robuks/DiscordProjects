abf623e4a5aac98a1c0a602626e88b2a3e02caa9
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2023-03-25 00:34:33 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,8 +11,9 @@ time.sleep(.2)
 print(Fore.GREEN + "[CONSOLE]: Welcome, ", username,"!")
 time.sleep(.2)
 print(Fore.BLUE + "[CONSOLE]: Command Prompt Loaded")
+webhookforbot = input("Please insert the bots webhook url >>>")
 code_exe = input(">>>")
-if code_exe == "deletewebhook":
+if code_exe == "1":
     webhookdelete = input("Input Discord Webhook >>>")
     webhook_url = webhookdelete
     response = requests.delete(webhook_url)
@@ -20,5 +21,32 @@ if code_exe == "deletewebhook":
         print("Webhook deleted successfully.")
 else:
     print("Error deleting webhook. Status code:", response.status_code)
-    
+
+if code_exe == "2":
+    webhook_url = webhookforbot
+    print(Fore.BLUE + "[CONSOLE]: Welcome to embed builder!")
+    title = input("Webhook Title >>>")
+    desc = input("Webhook Description >>>")
+
+embed = {
+    "title": title,
+    "description": desc,
+    "color": 16711680
+}
+
+# Define the payload data (including the embed)
+data = {
+    "username": "Embed built with Discord Fucker <3",
+    "avatar_url": "https://imgur.com/7tv3Wjs",
+    "embeds": [embed]
+}
+
+# Send a POST request to the webhook URL with the payload data
+response = requests.post(webhook_url, json=data)
+
+# Check the response status code
+if response.status_code == 200:
+    print("Message sent successfully.")
+else:
+    print("Error sending message. Status code:", response.status_code)
 
